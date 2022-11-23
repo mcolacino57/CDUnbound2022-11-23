7b6df6cdf4c0dbd67c358b3df5e0bd81f28fcec2
Michael COLACINO <mcolacino@mac.com>
2022-11-23 15:19:53 -0500

    Changed to local db

--- globals.js
+++ globals.js
@@ -2,7 +2,7 @@
 /*global Utilities , databaseC, propListC */
 const userEmail = "propgen@squarefoot.com"
 //const userEmail = getEmailFromJSON();
-const databaseNameG = "applesmysql";
+const databaseNameG = "applesmysql_loc";
 const todayS = Utilities.formatDate(new Date(), "GMT-4", "yyyy-MM-dd");
 const nowS = Utilities.formatDate(new Date(), "GMT-4", "yyyy-MM-dd HH:MM:ss");
 
