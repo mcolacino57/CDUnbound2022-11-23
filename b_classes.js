77bc0618cf1dc1c9e545296fcb57ac404cfe743d
Michael COLACINO <mcolacino@mac.com>
2022-11-25 00:03:07 -0500

    Removed propDetailC, ckSectionAC  ckLocalSectionAC

--- b_classes.js
+++ b_classes.js
@@ -4,59 +4,8 @@ getProposalNamesAndIDs , getPropSize , getPropLocation , createPropDetailA ,
 getPropStructFromName , matchProposalSizeWithClause */
 /*exported ckStringC, proposalC,brokerC,docC,responseC,databaseC, propListC   */
 
-class propDetailC {
-  constructor(dbInst, propID) {
-    this.propDetailA = createPropDetailA(dbInst, propID);
-  }
-  getAnswerFromCK(ckS) {
-    var found = this.propDetailA.find(c => c.ck === ckS);
-    return found?.ans
-  }
-}
 
-class ckSectionAC {
-  constructor() {
-    this.expA = ['oePerInc', 'oeBaseYear', 'retBaseYear', 'elecDirect', 'elecSubmeter'];
-    this.optA = ['optRenew', 'optYears', 'optROFO', 'optROFR'];
-    this.overA = ['secDeposit', 'useType', 'llName', 'llbrokerName', 'proposalSalutation', 'recipientEmail', 'llbrokerCo', 'llbrokerAddr', 'commDate', 'leaseTerm', 'earlyAccess'];
-    this.parkA = ['parkUnreservedNum', 'parkUnreservedRatio', 'parkUnreservedCost', 'parkReservedNum',
-      'parkReservedRatio', 'parkReservedCost', 'parkMaxEscPercent', 'parkDescription'];
-    this.tiA = ['tiAllow', 'tiFreight', 'tiAccess', 'tiCompBid', 'llWork'];
-  }
 
-  getExpA() {
-    return this.expA
-  }
-  getOptA() {
-    return this.optA
-  }
-  getOverA() {
-    return this.overA
-  }
-  getParkA() {
-    return this.parkA
-  }
-  getTIA() {
-    return this.tiA
-  }
-}
-
-class ckLocalSectionAC extends ckSectionAC {
-  getExpA(loc) {
-    var e = super.getExpA();
-    if (loc === "New York") {
-      e.push('elecRentInc');
-      e.push('elecRentIncCharge');  
-    }
-    return e 
-  }
-  getParkA(loc) {
-    if (loc === "New York") {
-      return []
-    }
-    return super.getParkA()
-  }
-}
 
 class ckC {
   constructor(dbInst, ck, proposalSize, proposalLocation, version) {
