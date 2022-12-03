4c0d4ffda7e32534073d120c91e29abf88fbff49
Michael COLACINO <mcolacino@mac.com>
2022-12-03 10:42:35 -0500

    Refactored, added

---
+++ clientSetProposalCurrent.js
@@ -0,0 +1,35 @@
+/**
+ * Purpose: set the current proposal with only a proposal
+ * name string; create a new propInst and dbInst to do so
+ *
+ * @param  {String} pnS - proposal name string
+ * @return {boolean} f or ID of the proposal - or throws error
+ */
+// eslint-disable-next-line no-unused-vars
+function clientSetProposalCurrent(pnS) {
+  var fS = "clientSetProposalCurrent";
+  var dbInst;
+  Logger.log(`Getting into clientSetProposalCurrent with ${pnS}`);
+  try {
+    const dbInst = dbInstG;
+    const propListInst = propListInstG;
+    // propInst = new proposalC(dbInst, pnS);
+    // var pid = propInst.getID();
+    const pid = propListInst.getIDfromName(pnS);
+    propListInst.setCurr(pid);
+    var ret = setProposalCurrent(dbInst, pid);
+    if (!ret)
+      throw new Error(`problem in setProposalCurrent for ${pnS}`);
+    var overDA = clientGetCDData(pnS);
+    if (!ret)
+      throw new Error(`problem in clientGetCDData for ${pnS}`);
+
+
+  } catch (err) {
+    Logger.log(`In ${fS}: ${err}`);
+    dbInst.closeconn();
+    return false;
+  }
+  dbInst.closeconn();
+  return overDA;
+}
